7d3c6b0036fecd2df45c215cc7d98d2a75460750
Steinarth <steinart16@ru.is>
2017-10-15 21:45:48 +0000

    print out when only one number

--- src/main/java/is/ru/hugb/stringcalculator/Calculator.java
+++ src/main/java/is/ru/hugb/stringcalculator/Calculator.java
@@ -3,6 +3,13 @@ package is.ru.stringcalculator;
 public class Calculator {
   public static int add(String text){
     if(text.equals( "")) {return 0;}
+    
+    String numbers[] = text.split(",");
+    if(numbers.length == 1) {return toInt(numbers[0]);}
     return 0;    
+  }
+
+  private static int toInt(String number){
+    return Integer.parseInt(number);
   }
 }
